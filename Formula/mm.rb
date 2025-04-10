eb55d78fccb1565b02e15e515b6b0b97ce4eb368
Zimeng Xiong (asahi-air) <zxzimeng@gmail.com>
2025-04-10 15:52:08 -0700

    update mm link

--- Formula/mm.rb
+++ Formula/mm.rb
@@ -1,7 +1,7 @@
 class Mm < Formula
   desc "A simple molecular weight calculator implemented in Rust"
   homepage "https://github.com/zimengxiong/cli-molecular-weight-calculator"
-  url "https://github.com/zxzimeng/cli-molecular-weight-calculator/releases/download/v0.1.1/v0.1.1.tar.gz"
+  url "https://github.com/ZimengXiong/cli-molecular-weight-calculator/releases/download/v0.1.1/mm-macos.tar.gz"
   sha256 "f7dc2967b904fc166094669e1d53deedcfd8b091b264d187d3f0213c64060d84"
 
   def install
