e05598e350d9bd22d77837ec1fa4991c72a9ebc5
Zimeng Xiong (asahi-air) <zxzimeng@gmail.com>
2025-04-10 22:10:24 -0700

    update cpp_test version

--- Formula/cpp_test.rb
+++ Formula/cpp_test.rb
@@ -2,7 +2,7 @@ class CppTest < Formula
   desc "CPP Tester supporting testcases and comparing alternative algorithims"
   homepage "https://github.com/ZimengXiong/cpp_test"
   url "https://github.com/ZimengXiong/cpp_test/releases/download/v0.1.0/cpp_test-macos.tar.gz"
-  sha256 "909b07af4aaca585dc08d7fd5c47fe7b8ba672b5547ebba9c68f2ca1cd9176a3"
+  sha256 "28e250c0739434282c4e82300124d58e27ba05b66a3652525a49d7134cc210cc"
   license "MIT"
 
   def install
