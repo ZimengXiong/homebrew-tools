b9e4dcfa591a559860c5ac929dc7cc97f3e3c5f5
Zimeng Xiong <zxzimeng@gmail.com>
2025-06-24 16:15:05 -0700

    add right click menubar

---
+++ Casks/right-click-menubar.rb
@@ -0,0 +1,16 @@
+cask "right-click-menubar" do
+  version "0.1.0"
+  sha256 :no_check
+
+  url "https://github.com/ZimengXiong/right-click-menubar/releases/download/V0.1.0/Right.Click.Menubar.app.zip"
+  name "Right Click Menubar"
+  desc "Access to the Application Menu in Context Menu at Cursor"
+  homepage "https://github.com/ZimengXiong/right-click-menubar"
+
+  app "Right Click Menubar.app"
+
+  zap trash: [
+    "~/Library/Application Support/Right Click Menubar",
+    "~/Library/Preferences/com.zimengxiong.right-click-menubar.plist"
+  ]
+end
