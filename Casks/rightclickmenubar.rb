3272d964188ea3384f516a22782cc39ab6c7f671
Zimeng Xiong <zxzimeng@gmail.com>
2025-06-24 18:00:21 -0700

    Update rightclickmenubar.rb

--- Casks/rightclickmenubar.rb
+++ Casks/rightclickmenubar.rb
@@ -3,18 +3,18 @@ cask "rightclickmenubar" do
   sha256 :no_check
 
   url "https://github.com/ZimengXiong/right-click-menubar/releases/download/V0.1.0/RightClickMenubar.zip"
-  name "Right Click Menubar"
+  name "RightClickMenubar"
   desc "Access to the Application Menu in Context Menu at Cursor"
   homepage "https://github.com/ZimengXiong/right-click-menubar"
 
-  app "Right Click Menubar.app"
+  app "RightClickMenubar.app"
 
   postflight do
-    system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{appdir}/Right Click Menubar.app"]
+    system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{appdir}/RightClickMenubar.app"]
   end
 
   zap trash: [
-    "~/Library/Application Support/Right Click Menubar",
-    "~/Library/Preferences/com.zimengxiong.right-click-menubar.plist"
+    "~/Library/Application Support/RightClickMenubar",
+    "~/Library/Preferences/com.zimengxiong.rightclickmenubar.plist"
   ]
 end
