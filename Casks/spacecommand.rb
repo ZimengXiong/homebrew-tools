45af85361305b14db17525bc242a85c592af4c85
Zimeng Xiong <zxzimeng@gmail.com>
2025-12-04 18:28:16 -0800

    update build

--- Casks/spacecommand.rb
+++ Casks/spacecommand.rb
@@ -1,6 +1,6 @@
 cask "spacecommand" do
   version "0.1.1"
-  sha256 "ad1e94e5d17a8d276105807c78690e5d0c3af84fea806b6fc6038f25914d5371"
+  sha256 "bb89e514fcd64b60ca91a50167953882f314440fc4070b0dda223e9bd9b791b2"
 
   url "https://github.com/ZimengXiong/SpaceCommand/releases/download/v#{version}/SpaceCommand.app.zip"
   name "SpaceCommand"
