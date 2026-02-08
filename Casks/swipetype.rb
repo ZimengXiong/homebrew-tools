bd499cccfabe087ade88d67f8933598a893c8b69
Zimeng Xiong <zxzimeng@gmail.com>
2026-02-08 11:50:12 -0800

    swipetype 0.1.3

--- Casks/swipetype.rb
+++ Casks/swipetype.rb
@@ -1,6 +1,6 @@
 cask "swipetype" do
-  version "0.1.2"
-  sha256 "0c2f02a5f15ef1b64cf2c3757c39f117848782dfe0fde4c67f44fc595f74dd92"
+  version "0.1.3"
+  sha256 "59b146b8637554d091f9132a50ecbfbf96930966397374be0d8938b641b2b50b"
 
   url "https://github.com/ZimengXiong/swipeType/releases/download/v#{version}/SwipeType.dmg"
   name "SwipeType"
